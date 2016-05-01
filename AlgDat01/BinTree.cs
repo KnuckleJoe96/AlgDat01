4ae1cf08877b37bf54dd6107abc85c108bdde861
KnuckleJoe96 <luk_popp@yahoo.de>
2016-05-01 14:05:00 +0200

    bintree - delete

--- AlgDat01/BinTree.cs
+++ AlgDat01/BinTree.cs
@@ -5,100 +5,130 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace AlgDat01 {
-  class BinTree : SortedSet {
-    Elem root;
-    int counter = 1;
+    class BinTree : SortedSet {
+        Elem root;
+        int counter = 1;
 
-    public override bool ExecuteInsert(int Value) {
-      counter++;
-      Elem newElem = new Elem() { value = Value };
-      bool found = false;
-      Elem temp = root;
-      while (found == false) {
-        if (Value < temp.value) {
-          if (temp.left != null)
-            temp = temp.left;
-          else {
-            temp.left = newElem;
-            found = true;
-          }
-        }
-        else {
-          if (temp.right != null)
-            temp = temp.right;
-          else {
-            temp.right = newElem;
-            found = true;
-          }
-        }
-      }
-      return true;
-    }
-
-    public override bool Search(int Value) {
-      int tempcount = 0;
-      Elem temp = root;
-      bool found = false;
-      while (found == false || tempcount < counter) {
-        if (Value == temp.value)
-          return true;
-        if (Value < temp.value) {
-          if (temp.left != null) {
-            tempcount++;
-            temp = temp.left;
-          }
-          else
-            return false;
-        }
-        else {
-          if (temp.right != null) {
-            tempcount++;
-            temp = temp.right;
-          }
-          else
-            return false;
-        }
-      }
-      return false;
-    }
-
-    public override bool Delete(int Value) {
-      if (Search(Value) == true) {
-        int tempcount = 0;
-        Elem father = root;
-        Elem temp = root;
-        bool found = false;
-        while (found == false || tempcount < counter) {
-          if (Value == temp.value)
+        public override bool ExecuteInsert(int Value) {
+            Elem newElem = new Elem() { value = Value };
+            Elem temp = root;
+            counter++;
+            bool found = false;
+            while (found == false) {
+                if (Value < temp.value) {
+                    if (temp.left != null)
+                        temp = temp.left;
+                    else {
+                        temp.left = newElem;
+                        found = true;
+                    }
+                }
+                else {
+                    if (temp.right != null)
+                        temp = temp.right;
+                    else {
+                        temp.right = newElem;
+                        found = true;
+                    }
+                }
+            }
             return true;
-          if (Value < temp.value) {
-            if (temp.left != null) {
-              tempcount++;
-              father = temp;
-              temp = temp.left;
-            }
-            else
-              return false;
-          }
-          else {
-            if (temp.right != null) {
-              tempcount++;
-              father = temp;
-              temp = temp.right;
-            }
-            else
-              return false;
-          }
         }
-        father = temp.left;
-        return false;
-      }
-    }
 
-    class Elem {
-      public int value;
-      public Elem right;
-      public Elem left;
+        public override bool Search(int Value) {
+            Elem temp = root;
+            int tempcount = 0;
+            bool found = false;
+            while (found == false || tempcount < counter) {
+                if (Value == temp.value)
+                    return true;
+                if (Value < temp.value) {
+                    if (temp.left != null) {
+                        tempcount++;
+                        temp = temp.left;
+                    }
+                    else
+                        return false;
+                }
+                else {
+                    if (temp.right != null) {
+                        tempcount++;
+                        temp = temp.right;
+                    }
+                    else
+                        return false;
+                }
+            }
+            return false;
+        }
+
+        public override bool Delete(int Value) {
+            if (Search(Value) == true) {
+                Elem father = root;
+                Elem temp = root;
+                if(temp.value == Value) {
+                    
+                        
+                }
+                bool found = false;
+
+                while (found == false) {
+                    if (Value == temp.value)
+                        return true;
+                    if (Value < temp.value) {
+                        if (temp.left != null) {
+                            father = temp;
+                            temp = temp.left;
+                        }
+                        else
+                            return false;
+                    }
+                    else {
+                        if (temp.right != null) {
+                            father = temp;
+                            temp = temp.right;
+                        }
+                        else
+                            return false;
+                    }
+                }
+                father = temp.left;
+                return false;
+            }
+            else {
+                return false;
+            }
+        }
+
+        Elem findSymmetricalSuccessor(Elem e) {
+            Elem temp = e.left;
+            bool finished = false;
+
+            while (!finished) {
+                if (temp.right != null) {
+                    temp = temp.right;
+                }
+                else {
+                    finished = true;
+                }
+            }
+            return temp;
+        }
+
+        int checkForSuccessors(Elem e) {
+            if (e.left != null && e.right != null)
+                return 2;
+
+            if (e.left != null || e.right != null)
+                return 1;
+
+            return 0;
+        }
+
+        class Elem {
+            public int value;
+            public Elem right;
+            public Elem left;
+        }
     }
-  }
 }
